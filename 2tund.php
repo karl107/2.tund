3efc0f02a7d8c5bafd413adc934acc343b01019e
karlkruu <karlkruu@greeny.cs.tlu.ee>
2016-09-13 13:19:27 +0300

    for loop & if statement

---
+++ 2tund.php
@@ -0,0 +1,26 @@
+<?php
+
+	$age = 5;
+	
+	if ($age < 18){
+		echo "alaealine";
+	}else{
+		echo "täisealine";
+	}
+	
+
+?>
+<br>
+
+<?php
+
+	// eesmärk vanusele vastavalt
+	// trükime välja 'palju'
+
+	for($i = 1; $i <= $age; $i = $i + 1){
+		echo $i."palju "; //$i ees lisab numbri 'palju' ette
+		
+	}
+	echo "õnne!";
+	
+?>
